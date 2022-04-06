a3b158315e41a340ae5cf163e85c8a9d64143078
billytrinh <quanghoa.trinh@gmail.com>
2022-04-06 09:40:14 +0700

    demo javafx search data

--- src/javafx/ListController.java
+++ src/javafx/ListController.java
@@ -1,5 +1,7 @@
 package javafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -7,12 +9,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ListController implements Initializable {
 //    public Text listView;
@@ -21,6 +28,8 @@ public class ListController implements Initializable {
     public TableColumn<Person,String> cEmail;
     public TableColumn<Person,Integer> cAge;
     public TableColumn<Person, Button> cEdit;
+
+    public TextField txtSearch;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -34,5 +43,16 @@ public class ListController implements Initializable {
     public void formScreen() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("form.fxml"));
         Main.rootStage.setScene(new Scene(root,800,600));
+    }
+
+    public void search(){
+        String s = txtSearch.getText();
+        List<Person> kq = Main.personList.stream()
+                                .filter(p->p.getFullname().contains(s)
+                                        || p.getEmail().contains(s))
+                                .collect(Collectors.toList());
+        ObservableList result = FXCollections.observableArrayList();
+        result.addAll(kq);
+        tbView.setItems(result);
     }
 }
